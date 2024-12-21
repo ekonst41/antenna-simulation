fa8ddf3bb2ca361073a1f8f114c8f6ef0e3cabd6
danilkadyrov <kadyrov.da@phystech.edu>
2024-12-21 22:17:41 +0300

    Add notebook for results, changed code for parallel wave

--- antenna.py
+++ antenna.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 class Antenna:
   def __init__(self, x0: float, y0: float, diameter: float, focal_length: float):
@@ -11,9 +10,6 @@ class Antenna:
       self.x_min = x0 - diameter / 2
       self.x_max = x0 + diameter / 2
 
-    #def check_collision(self, x: float, y: float):
-    #    y_parabola = (x - self.x0) ** 2 / (4 * self.focal_length) + self.y0
-    #    return np.isclose(y, y_parabola)
 
   def antenna_equation(self, x: float):
     return self.x_min <= x <= self.x_max and (x - self.x0) ** 2 / (4 * self.focal_length) + self.y0
--- mesh.py
+++ mesh.py
@@ -9,8 +9,8 @@ import imageio.v2 as imageio
 
 frames = []
 
-epsilon_0 = 8.854e-12
-mu_0 = 4e-7 * np.pi
+EPS_0 = 8.854e-12
+MU_0 = 4e-7 * np.pi
 
 def max_except_source(E: np.ndarray, i: List[int], j: List[int]):
   mask = np.ones(E.shape, dtype=bool)
@@ -57,7 +57,7 @@ class ElectoMagneticMesh:
   def _calculate_fields(self, dt: float = 0.0025 / (2 * 3e8)):
     for i in range(1, self.grid_size):
         for j in range(1, self.grid_size):
-            self.Hz_next[i, j] -= (dt  / mu_0) * ((self.Ey[i, j] - self.Ey[i, j - 1]) / self.dx - (self.Ex[i, j] - self.Ex[i - 1, j]) / self.dy)
+            self.Hz_next[i, j] -= (dt  / MU_0) * ((self.Ey[i, j] - self.Ey[i, j - 1]) / self.dx - (self.Ex[i, j] - self.Ex[i - 1, j]) / self.dy)
 
     for i in range(self.grid_size - 1):
       for j in range(self.grid_size - 1):
@@ -68,8 +68,8 @@ class ElectoMagneticMesh:
           self.Ex_next[i, j] = 0
           self.Ey_next[i, j] = 0
           continue
-        self.Ex_next[i, j] += (dt / epsilon_0) * ((self.Hz[i + 1, j] - self.Hz[i, j]) / self.dy)
-        self.Ey_next[i, j] -= (dt / epsilon_0) * ((self.Hz[i, j + 1] - self.Hz[i, j]) / self.dx)
+        self.Ex_next[i, j] += (dt / EPS_0) * ((self.Hz[i + 1, j] - self.Hz[i, j]) / self.dy)
+        self.Ey_next[i, j] -= (dt / EPS_0) * ((self.Hz[i, j + 1] - self.Hz[i, j]) / self.dx)
         
         self.Ex = np.copy(self.Ex_next)
         self.Ey = np.copy(self.Ey_next)
@@ -78,20 +78,20 @@ class ElectoMagneticMesh:
   def visualize(self, num_steps: int = 100, dt: float = 0.0025 / (2 * 3e8)):
     for source in self.sources: 
       self.Ex[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = source.source_func(0)
-      self.Hz[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = np.sqrt(epsilon_0 / mu_0) * source.source_func(0)
+      self.Hz[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = np.sqrt(EPS_0 / MU_0) * source.source_func(0)
 
     for t in tqdm(range(num_steps)):
       self._calculate_fields()
           
       if t < num_steps // 5:
         for source in self.sources:
-          self.Ex[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = source.source_func(t * dt)  # Применение источника
-          self.Hz[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = np.sqrt(epsilon_0 / mu_0) * source.source_func(t * dt)
+          self.Ex[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = source.source_func(t * dt)
+          self.Hz[int(source.y0 / self.dy), int(source.x0 / self.dx) + self.grid_size//2] = np.sqrt(EPS_0 / MU_0) * source.source_func(t * dt)
 
       if t % 10 == 0:
         fig, ax = plt.subplots(figsize=(6, 6))
-        ax.set_xlim(0, self.grid_size * self.dx)  # Установка масштаба по оси X
-        ax.set_ylim(0, self.grid_size * self.dy)  # Установка масштаба по оси Y
+        ax.set_xlim(0, self.grid_size * self.dx)
+        ax.set_ylim(0, self.grid_size * self.dy)
         ax.set_xlabel("X (м)")
         ax.set_ylabel("Y (м)")
         ax.set_title("Электромагнитное поле")
@@ -116,10 +116,7 @@ class ElectoMagneticMesh:
         ax.set_xlabel("X (м)")
         ax.set_ylabel("Y (м)")
         ax.axis("equal")
-
-          # Обновление графика
-          #plt.draw()
-          #plt.pause(0.01)  # Пауза для обновления
+        
         plt.savefig('temp.png')
         plt.close()
         frames.append(imageio.imread('temp.png'))
