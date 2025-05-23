54648751aba4fac4135d7731700fda265ad23658
elina.konstantinova <konstantinova.em@phystech.edu>
2025-05-23 23:24:52 +0300

    Add runner and visualize function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from tests.test_davis import test_davis 
+from tests.test_davis import test_davis
+import numericalunits as nu
+from tests.runner import run
+
+search_domain_kx = [-0.05/nu.nm, 0.05/nu.nm, 0, 0.4/nu.nm]
+path = 'davis.yaml'
 
 if __name__ == "__main__":
-    test_davis()
+    #test_davis()
+    run(path=path, search_domain_kx=search_domain_kx, num_to_visualize=5)
